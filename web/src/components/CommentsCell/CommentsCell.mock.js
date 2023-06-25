371e5872d80ada22de8892e54feec887a76e4ed4
benstraw <ben@benstrawbridge.com>
2023-06-25 01:40:08 -0700

    moved everything over from that old spot yo. also BOOM.

---
+++ web/src/components/CommentsCell/CommentsCell.mock.js
@@ -0,0 +1,19 @@
+// Define your own mock data here:
+export const standard = (/* vars, { ctx, req } */) => ({
+  comments: [
+    {
+      id: 42,
+      name: 'Moose McDuck',
+      body: 'Your site is wack',
+      postId: 1,
+      createdAt: '2021-03-11T19:56:39.492Z',
+    },
+    {
+      id: 43,
+      name: 'Mindy McDuck',
+      body: 'I think crack is wack',
+      postId: 1,
+      createdAt: '2021-03-11T19:56:49.492Z',
+    },
+  ],
+})
---
+++ web/src/pages/TermsPage/TermsPage.stories.js
@@ -0,0 +1,10 @@
+import TermsPage from './TermsPage'
+
+export const generated = () => {
+  return <TermsPage />
+}
+
+export default {
+  title: 'Pages/TermsPage',
+  component: TermsPage,
+}
