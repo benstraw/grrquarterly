eb68eb2e7fc13d11047ed0eae730a865e4d9e9e9
benstraw <ben@benstrawbridge.com>
2023-07-03 14:47:31 -0700

    small clean up on post

--- web/src/components/Post/Post/Post.js
+++ web/src/components/Post/Post/Post.js
@@ -2,6 +2,8 @@ import { Link, routes, navigate } from '@redwoodjs/router'
 import { useMutation } from '@redwoodjs/web'
 import { toast } from '@redwoodjs/web/toast'
 
+import { timeTag } from 'src/lib/formatters'
+
 const DELETE_POST_MUTATION = gql`
   mutation DeletePostMutation($id: Int!) {
     deletePost(id: $id) {
@@ -9,26 +11,6 @@ const DELETE_POST_MUTATION = gql`
     }
   }
 `
-
-const jsonDisplay = (obj) => {
-  return (
-    <pre>
-      <code>{JSON.stringify(obj, null, 2)}</code>
-    </pre>
-  )
-}
-
-const timeTag = (datetime) => {
-  return (
-    <time dateTime={datetime} title={datetime}>
-      {new Date(datetime).toUTCString()}
-    </time>
-  )
-}
-
-const checkboxInputTag = (checked) => {
-  return <input type="checkbox" checked={checked} disabled />
-}
 
 const Post = ({ post }) => {
   const [deletePost] = useMutation(DELETE_POST_MUTATION, {
